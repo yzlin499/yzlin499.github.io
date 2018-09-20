ebd7354d1532252cc5939013341a8b8227f1b659
yzlin <499680328@qq.com>
2018-09-20 23:10:33 +0800

    更新明天的课上实验

---
+++ homework/demo2/demo2.js
@@ -0,0 +1,20 @@
+new Vue({
+    el: '#info',
+    data: {
+        prizeInfo: [
+            {
+                title: '优秀教材一等奖(68种)',
+                books: [
+                    '刑事诉讼法（第五版），陈光中主编，北京大学出版社',
+                    '普通化学原理（第4版），华'
+                ]
+            },
+            {
+                title: 'asddsa',
+                books: ['asddsadasdas', 'adsdsadsadsa']
+            }
+        ]
+    }
+
+
+});
