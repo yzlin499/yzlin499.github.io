c3f31fdbc933ab0b5fe15ed801a7563ed352652a
yzlin <499680328@qq.com>
2018-09-20 22:35:39 +0800

    更新明天的课上实验

---
+++ js/demo_1.js
@@ -0,0 +1,22 @@
+var listVue = new Vue({
+    el: '#list',
+    data: {
+        windowsSystems: [
+            {name: 'windows 95'},
+            {name: 'windows 98'},
+            {
+                name: 'windows NT',
+                other: ['windows NT Workstation', 'windows NT Server']
+            },
+            {
+                name: 'windows 2000',
+                other: ['windows 2000 Professional', 'windows 2000 Server', 'windows 2000 Advance Server']
+            },
+            {name: 'windows xp'},
+            {name: 'windows vista'},
+            {name: 'windows 7'},
+            {name: 'windows 8'}
+        ]
+    }
+});
+
